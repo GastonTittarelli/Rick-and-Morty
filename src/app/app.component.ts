b83c2934f98b751e0600dc41ffc132e1dd80ae97
GastonTittarelli <116087636+GastonTittarelli@users.noreply.github.com>
2025-05-21 18:13:10 -0300

    bootstrap added, rick and morty api added in service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { CharactersComponent } from './components/characters.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgbModule, CharactersComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
   title = 'mi-proyecto-angular';
 }
---
+++ src/app/components/characters.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { RickAndMortyService } from '../../service/rick-and-morty.service';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'characters-app',
+  imports: [CommonModule],
+  templateUrl: './characters.component.html'
+})
+export class CharactersComponent implements OnInit {
+  personajes: any[] = [];
+
+  constructor(private rickService: RickAndMortyService) {}
+
+  ngOnInit(): void {
+    this.rickService.getAllCharacters().subscribe((data: any) => {
+      this.personajes = data.results;
+    });
+  }
+}
