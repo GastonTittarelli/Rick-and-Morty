8ab19c2e26f768cd100b37c02636e5bcfa0ead8e
GastonTittarelli <116087636+GastonTittarelli@users.noreply.github.com>
2025-05-27 20:30:36 -0300

    mobile view added

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -6,8 +6,23 @@ import { RouterModule } from '@angular/router';
   selector: 'app-nav-bar',
   imports: [CommonModule, RouterModule],
   templateUrl: './nav-bar.component.html',
-  styleUrl: './nav-bar.component.css'
+  styleUrl: './nav-bar.component.css',
 })
 export class NavBarComponent {
+  menuOpen = false;
+  disableTransition = false;
 
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+    this.disableTransition = false;
+  }
+
+  closeMenu() {
+    this.disableTransition = true;
+    this.menuOpen = false;
+
+    setTimeout(() => {
+      this.disableTransition = false;
+    });
+  }
 }
