82c9e304d663701f50ee91cae80cda742d2ca6fb
GastonTittarelli <116087636+GastonTittarelli@users.noreply.github.com>
2025-05-26 22:04:48 -0300

    404 view added and hover cards implement

--- src/app/views/not-found/not-found.component.ts
+++ src/app/views/not-found/not-found.component.ts
@@ -1,18 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-not-found',
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './not-found.component.html',
   styleUrl: './not-found.component.css'
 })
 export class NotFoundComponent {
 
-  constructor(private router: Router) {}
+  // constructor(private router: Router) {}
 
-  goBack() {
-      this.router.navigate(['/home']);
-  }
+  // goBack() {
+  //     this.router.navigate(['/home']);
+  // }
 
 }
