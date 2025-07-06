95fc877bf824c038d951a7681b6a0ea0843f823c
GastonTittarelli <116087636+GastonTittarelli@users.noreply.github.com>
2025-07-06 16:40:18 -0300

    Nest y Postgres con Docker funcionando

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { User } from './user/entities/user.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(), // con esto leo el .env
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST || 'localhost',
+      port: parseInt(process.env.DB_PORT || '5432', 10),
+      username: process.env.DB_USER || 'postgres',
+      password: process.env.DB_PASSWORD || 'postgres',
+      database: process.env.DB_NAME || 'teslo_db',
+      autoLoadEntities: true,
+      synchronize: true, // solo en desarrollo
+    }),
+    TypeOrmModule.forFeature([User]), 
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- server/src/app.service.ts
+++ server/src/app.service.ts
@@ -1,8 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './user/entities/user.entity';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepo: Repository<User>,
+  ) {}
+
+  async getUsers(): Promise<User[]> {
+    return this.userRepo.find();
+  }
+
+  async createUser(): Promise<User> {
+    const user = this.userRepo.create({
+      email: 'test@example.com',
+      password: '123456',
+    });
+    return this.userRepo.save(user);
   }
 }
