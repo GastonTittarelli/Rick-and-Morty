cb71331680cccdf647299419e326f6c45a233626
GastonTittarelli <116087636+GastonTittarelli@users.noreply.github.com>
2025-07-20 18:51:14 -0300

    persistence ll

--- server/src/main.ts
+++ server/src/main.ts
@@ -14,7 +14,7 @@ async function bootstrap() {
   );
 
   app.enableCors({
-    origin: 'http://localhost:4200',
+    origin: ['http://localhost:4200', 'https://rick-and-morty-blond-three.vercel.app' ],
     credentials: true,
   });
   await app.listen(process.env.PORT ?? 3000);
